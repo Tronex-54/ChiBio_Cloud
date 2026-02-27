b9d7fc89d7072d1c1f6d4420e446088bd807fcc5
tronex <juromerom@unal.edu.co>
2026-02-26 19:35:15 -0500

    Update

--- protocolo.py
+++ protocolo.py
@@ -1,1 +1,6 @@
-print("Prueba de comunicacion limpia")
+print("¡CONEXION EXITOSA!")
+print("Enviado desde el transmisor de Tronex-54")
+# Prueba de suma
+a = 5
+b = 10
+print("Resultado en el reactor:", a + b)
