3dec26fa0bec3b78843ab77ecf4a698f42b071a2
tronex <juromerom@unal.edu.co>
2026-02-27 13:35:04 -0500

    Update from Master Architect V6.8

--- protocolo.py
+++ protocolo.py
@@ -2,25 +2,21 @@
         current_status = sysData[M]['Custom']['Status']
 
         if current_status == 0.0:
-            sysData[M]['Thermostat']['target'] = 37
+            sysData[M]['Thermostat']['target'] = 45
             SetOutputOn(M, 'Thermostat', 1)
-            sysData[M]['Stir']['target'] = 0.5
-            SetOutputOn(M, 'Stir', 1)
             sysData[M]['OD']['target'] = 0.3
+            SetOutputOn(M, 'OD', 1)
             sysData[M]['Custom']['Status'] = 1.0
 
         elif current_status == 1.0:
-            if sysData[M]['ThermometerIR']['current'] == 37:
-                sysData[M]['Custom']['Status'] = 3.0
+            sysData[M]['Custom']['param1'] = sysData[M]['Experiment']['cycles']
+            sysData[M]['Custom']['Status'] = 3.0
         elif current_status == 3.0:
-            sysData[M]['LEDA']['target'] = 0.7
-            SetOutputOn(M, 'LEDA', 1)
-            time.sleep(2)
-            SetOutputOn(M, 'LEDA', 0)
-            sysData[M]['Pump1']['target'] = 0.5
-            SetOutputOn(M, 'Pump1', 1)
-            time.sleep(5)
-            SetOutputOn(M, 'Pump1', 0)
+            if (sysData[M]['Experiment']['cycles'] - sysData[M]['Custom']['param1']) >= 1:
+                sysData[M]['Custom']['Status'] = 4.0
+        elif current_status == 4.0:
+            sysData[M]['Thermostat']['target'] = 25
+            SetOutputOn(M, 'Thermostat', 1)
             sysData[M]['Custom']['Status'] = 2.0
         elif current_status == 2.0:
             addTerminal(M, 'Protocolo Finalizado')
