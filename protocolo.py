02e48b95d022b55e583ec086242941f62d835574
tronex <juromerom@unal.edu.co>
2026-02-26 19:40:03 -0500

    Real-time Update

--- protocolo.py
+++ protocolo.py
@@ -1,5 +1,5 @@
 # Escribe tu codigo aqui
 print("--- INICIO DE SECUENCIA ---")
-temperatura = 37.5
+temperatura = 37.555555555
 print(f"Ajustando a {temperatura} grados")
 print("--- FIN DE SECUENCIA ---")
