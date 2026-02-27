93220097f85d21dbb236fa204e23ccbc304b3b68
tronex <juromerom@unal.edu.co>
2026-02-26 19:42:35 -0500

    Real-time Update

--- protocolo.py
+++ protocolo.py
@@ -1,5 +1,34 @@
-# Escribe tu codigo aqui
-print("--- INICIO DE SECUENCIA ---")
-temperatura = 37.555555555
-print(f"Ajustando a {temperatura} grados")
-print("--- FIN DE SECUENCIA ---")
+    elif (program == "C8"):
+        current_status = sysData[M]['Custom']['Status']
+
+        if current_status == 0.0:
+            sysData[M]['Thermostat']['target'] = 37
+            SetOutputOn(M, 'Thermostat', 1)
+            sysData[M]['OD']['target'] = 0.3
+            SetOutputOn(M, 'Zigzag', 1)
+            sysData[M]['Zigzag']['Zig'] = 0.04
+            SetOutputOn(M, 'OD', 1)  # Zigzag requiere Turbidostato
+            sysData[M]['Stir']['target'] = 0.5
+            SetOutputOn(M, 'Stir', 1)
+            sysData[M]['Custom']['Status'] = 1.0
+
+        elif current_status == 1.0:
+            sysData[M]['Custom']['loop_nn7'] = 0
+            sysData[M]['Custom']['Status'] = 3.0
+        elif current_status == 3.0:
+            sysData[M]['LEDA']['target'] = 0.7
+            SetOutputOn(M, 'LEDA', 1)
+            time.sleep(15)
+            SetOutputOn(M, 'LEDA', 0)
+            sysData[M]['Custom']['Status'] = 4.0
+        elif current_status == 4.0:
+            sysData[M]['Custom']['loop_nn7'] += 1
+            if sysData[M]['Custom']['loop_nn7'] < 100:
+                sysData[M]['Custom']['Status'] = 3.0
+            else:
+                sysData[M]['Custom']['Status'] = 2.0
+        elif current_status == 2.0:
+            addTerminal(M, 'Protocolo Finalizado')
+            sysData[M]['Custom']['Status'] = 99.0
+        elif current_status == 99.0:
+            pass  # Reposo eterno
