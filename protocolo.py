48a024faee674be47599d84a70601987c168b57d
tronex <juromerom@unal.edu.co>
2026-02-27 14:48:00 -0500

    Update from Master Architect V6.8

--- protocolo.py
+++ protocolo.py
@@ -13,7 +13,14 @@
             SetOutputOn(M, 'Pump1', 1)
             time.sleep(5)
             SetOutputOn(M, 'Pump1', 0)
-            sysData[M]['Custom']['Status'] = 2.0
+            sysData[M]['LEDE']['target'] = 0.1
+            SetOutputOn(M, 'LEDE', 1)
+            sysData[M]['Custom']['param1'] = sysData[M]['Experiment']['cycles']
+            sysData[M]['Custom']['Status'] = 3.0
+        elif current_status == 3.0:
+            if (sysData[M]['Experiment']['cycles'] - sysData[M]['Custom']['param1']) >= 1:
+                SetOutputOn(M, 'LEDE', 0)
+                sysData[M]['Custom']['Status'] = 2.0
         elif current_status == 2.0:
             addTerminal(M, 'Protocolo Finalizado')
             sysData[M]['Custom']['Status'] = 99.0
