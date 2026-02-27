8d6c183e773a85b96c95b43d1aed695df621f224
tronex <juromerom@unal.edu.co>
2026-02-27 13:14:39 -0500

    Update from Master Architect V6.8

--- protocolo.py
+++ protocolo.py
@@ -10,6 +10,13 @@
             sysData[M]['Custom']['Status'] = 1.0
 
         elif current_status == 1.0:
+            if sysData[M]['ThermometerIR']['current'] == 37:
+                sysData[M]['Custom']['Status'] = 3.0
+        elif current_status == 3.0:
+            sysData[M]['LEDA']['target'] = 0.7
+            SetOutputOn(M, 'LEDA', 1)
+            time.sleep(2)
+            SetOutputOn(M, 'LEDA', 0)
             sysData[M]['Pump1']['target'] = 0.5
             SetOutputOn(M, 'Pump1', 1)
             time.sleep(5)
