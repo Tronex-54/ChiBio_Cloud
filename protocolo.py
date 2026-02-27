86bc0515c85163913a627401e9afddfa00a169b3
tronex <juromerom@unal.edu.co>
2026-02-27 13:01:47 -0500

    Update from Master Architect V6.8

--- protocolo.py
+++ protocolo.py
@@ -1,5 +1,13 @@
-# Escribe tu codigo aqui
-print("--- INICIO DE SECUENCIA ---")
-temperatura = 37.5
-print(f"Ajustando a {temperatura} grados")
-print("--- FIN DE SECUENCIA sdsd---")
+    elif (program == "C8"):
+        current_status = sysData[M]['Custom']['Status']
+
+        if current_status == 0.0:
+            sysData[M]['Thermostat']['target'] = 37
+            SetOutputOn(M, 'Thermostat', 1)
+            sysData[M]['Custom']['Status'] = 1.0
+
+        elif current_status == 2.0:
+            addTerminal(M, 'Protocolo Finalizado')
+            sysData[M]['Custom']['Status'] = 99.0
+        elif current_status == 99.0:
+            pass  # Reposo eterno
