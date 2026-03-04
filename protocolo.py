bbc32af8fc94fb0a6a63231c09f0f5ce06bc3c74
tronex <juromerom@unal.edu.co>
2026-03-04 13:38:54 -0500

    Update from Master Architect V6.8

--- protocolo.py
+++ protocolo.py
@@ -7,10 +7,10 @@
             sysData[M]['Custom']['Status'] = 1.0
 
         elif current_status == 1.0:
-            sysData[M]['LEDF']['target'] = 0.1
-            SetOutputOn(M, 'LEDF', 1)
+            sysData[M]['LEDG']['target'] = 0.1
+            SetOutputOn(M, 'LEDG', 1)
             time.sleep(5)
-            SetOutputOn(M, 'LEDF', 0)
+            SetOutputOn(M, 'LEDG', 0)
             sysData[M]['Custom']['Status'] = 2.0
         elif current_status == 2.0:
             addTerminal(M, 'Protocolo Finalizado')
