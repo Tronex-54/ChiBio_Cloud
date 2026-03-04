74c5bd51dc6f4fe403ff6e0ba59085551752c367
tronex <juromerom@unal.edu.co>
2026-03-04 16:23:16 -0500

    Update from Master Architect V6.8

--- protocolo.py
+++ protocolo.py
@@ -6,6 +6,15 @@
             SetOutputOn(M, 'Thermostat', 1)
             sysData[M]['Custom']['Status'] = 1.0
 
+        elif current_status == 1.0:
+            sysData[M]['LEDC']['target'] = 0.1
+            SetOutputOn(M, 'LEDC', 1)
+            sysData[M]['Custom']['param1'] = sysData[M]['Experiment']['cycles']
+            sysData[M]['Custom']['Status'] = 3.0
+        elif current_status == 3.0:
+            if (sysData[M]['Experiment']['cycles'] - sysData[M]['Custom']['param1']) >= 1:
+                SetOutputOn(M, 'LEDC', 0)
+                sysData[M]['Custom']['Status'] = 2.0
         elif current_status == 2.0:
             addTerminal(M, 'Protocolo Finalizado')
             sysData[M]['Custom']['Status'] = 99.0
